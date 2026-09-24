468aa5e79805eac166f8afdc6acfb881b624a8f0
fenixdonatti <fenixdonatti@gmail.com>
2026-09-24 10:20:53 -0300

    icon & og-image

--- app/app.vue
+++ app/app.vue
@@ -8,10 +8,60 @@
 
 <script lang="ts" setup>
 const config = useRuntimeConfig()
+
 const siteUrl = (config.public.siteUrl || useRequestURL().origin).replace(/\/$/, '')
 const personId = `${siteUrl}/#person`
 
 useHead({
+  title: 'Fênix Rodrigues Pedro | Desenvolvedor de Software',
+
+  meta: [
+    {
+      name: 'description',
+      content:
+        'Portfólio de Fênix Rodrigues Pedro, Desenvolvedor de Software. Conheça meus projetos, experiências e tecnologias.'
+    },
+
+    // Open Graph
+    {
+      property: 'og:title',
+      content: 'Fênix Rodrigues Pedro | Desenvolvedor de Software'
+    },
+    {
+      property: 'og:description',
+      content:
+        'Portfólio de Fênix Rodrigues Pedro, Desenvolvedor de Software. Conheça meus projetos, experiências e tecnologias.'
+    },
+    {
+      property: 'og:type',
+      content: 'website'
+    },
+    {
+      property: 'og:url',
+      content: siteUrl
+    },
+    {
+      property: 'og:image',
+      content: `${siteUrl}/og-image.jpg`
+    },
+    {
+      property: 'og:image:width',
+      content: '1200'
+    },
+    {
+      property: 'og:image:height',
+      content: '630'
+    },
+    {
+      property: 'og:image:alt',
+      content: 'Portfólio de Fênix Rodrigues Pedro'
+    },
+    {
+      property: 'og:locale',
+      content: 'pt_BR'
+    },
+  ],
+
   script: [
     {
       type: 'application/ld+json',
@@ -51,4 +101,3 @@ useHead({
   ],
 })
 </script>
-
